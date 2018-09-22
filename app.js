527fb23a8c0541f97b85294188e7279443be54ed
Kevin Navero <knavero@gmail.com>
2018-09-21 18:05:16 -0700

    app.js: point to angular dist

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const apiRouter = require('./routes/apiRouter');
+const dist = 'angular-frontend/dist/mean-app';
 
 const app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, dist)));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/api', apiRouter);
+
+app.use((req, res, next) => {
+   res.render(path.join(__dirname, dist, 'index.html'));
+})
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
